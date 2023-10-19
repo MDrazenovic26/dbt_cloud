61f629dff47170e8188545a4677650c54df211a4
Matija Draženović <matija.drazenovic@valcon.com>
2023-10-19 10:12:02 +0000

    dodani shipment, orderline i store sa izmjenjenom logikom od ostalih upita

--- models/integration/orderline.sql
+++ models/integration/orderline.sql
@@ -1,17 +1,118 @@
 {{
     config(
-        materialized= 'table'
+        materialized= 'table',
+        unique_key='ID',
+        incremental_strategy='merge'
     )
 }}
 
-with vbrp as (
-    select *
-    from {{ source('snf_raw', 'VBRP') }}
-)
+with orderline_original as (
+    select 
+        HASHID,
+        INDEX, 
+	    POSNR,
+	    VBELN,
+	    ORDDT, 
+	    MANDT,
+	    MATNR,
+	    STKPR, 
+	    WAERS,
+	    FKIMG, 
+	    SKTOF,
+	    KPEIN, 
+	    TIMESTAMP,
+	    PERNR,
+	    BEZNR,
+	    RSNR,
+        INSERT_DATE,
+        LAST_MODIFIED
+    FROM
+        {{ source('snf_raw', 'VBRP') }}
 
-{{ dbt_utils.deduplicate(
-    relation='vbrp',
-    partition_by='INDEX, POSNR',
-    order_by='ORDDT desc,timestamp desc',
-   )
-}}
+    {% if is_incremental() %}
+        where INSERT_DATE > (select max(INSERT_DATE) from {{this}}) 
+    {%endif%}
+),
+ORDERLINE_UPDATES AS (
+    SELECT
+        HASHID,
+        INDEX,  
+	    POSNR,
+	    VBELN,
+	    ORDDT, 
+	    MANDT,
+	    MATNR,
+	    STKPR, 
+	    WAERS,
+	    FKIMG, 
+	    SKTOF,
+	    KPEIN, 
+	    TIMESTAMP,
+	    PERNR,
+	    BEZNR,
+	    RSNR,
+        INSERT_DATE,
+        LAST_MODIFIED
+    FROM 
+        orderline_original
+    
+    {% if is_incremental() %}
+        where HASHID IN (SELECT ID FROM {{this}})
+    {% endif %}
+),
+ORDERLINE_INSERTS AS (
+    SELECT
+        HASHID,
+        INDEX,  
+	    POSNR,
+	    VBELN,
+	    ORDDT, 
+	    MANDT,
+	    MATNR,
+	    STKPR, 
+	    WAERS,
+	    FKIMG, 
+	    SKTOF,
+	    KPEIN, 
+	    TIMESTAMP,
+	    PERNR,
+	    BEZNR,
+	    RSNR,
+        INSERT_DATE,
+        LAST_MODIFIED
+    FROM 
+        orderline_original
+    
+    {% if is_incremental() %}
+        where HASHID NOT IN (SELECT HASHID FROM ORDERLINE_UPDATES)
+    {% endif %}
+),
+ORDERLINE_UNION AS (
+    SELECT * FROM ORDERLINE_UPDATES
+    UNION
+    SELECT * FROM ORDERLINE_INSERTS
+),
+ORDERLINE_FINAL AS (
+    SELECT
+        HASHID AS ID,
+	    POSNR AS ORDER_LINE_ID,
+	    VBELN AS ORDER_ID,
+	    ORDDT AS ORDER_DATE, 
+	    MANDT AS CUSTOMER_ID,
+	    MATNR AS PRODUCT_ID,
+	    STKPR AS UNIT_PRICE, 
+	    WAERS AS CURRENCY_CODE,
+	    FKIMG AS QUANTITY, 
+	    SKTOF AS DISCOUNT_ID,
+	    KPEIN AS NET_PRICE, 
+	    TIMESTAMP AS TIMESTAMP,
+	    PERNR AS EMPLOYEE_ID,
+	    BEZNR AS PAYMENT_ID,
+	    RSNR AS STORE_ID,
+        CURRENT_DATE() AS INSERT_DATE,
+        CURRENT_DATE() AS LAST_MODIFIED
+    FROM 
+        ORDERLINE_UNION
+)
+SELECT * FROM ORDERLINE_FINAL
+
